bb65dcb0c0b610cec0bd568c6831398b3921a9f2
ed1ct <edik.tigranyan@yandex.ru>
2024-05-11 19:36:35 +0300

    game_over_checker update 1.0

--- Krestiki Noliki/Krestiki Noliki/Krestiki Noliki.cpp
+++ Krestiki Noliki/Krestiki Noliki/Krestiki Noliki.cpp
@@ -8,15 +8,15 @@ using namespace std;
 class krestici_noliki {
 public:
     krestici_noliki(bool first_move) {
-        this->your_move = first_move;
+        this->move_rigth = first_move;
         if (first_move) {
-            Pkr_or_nl = 1;
+            Player = 1;
         }
         else {
-            Pkr_or_nl = 2;
+            Player = 2;
         }
+        
         cout << "place your sign";
-
         cout << "0 - left_up" << endl;
         cout << "1 - center_up" << endl;
         cout << "2 - right_up" << endl;
@@ -30,68 +30,104 @@ public:
     }
 
     void player_move() {
-        while (your_move) {
+        while (move_rigth) {
             cin >> position;
-            if (polEEE[position] == 0) {
-                polEEE[position] = Pkr_or_nl;
-                your_move = false;
+            if (pfield[position] == 0) {
+                pfield[position] = Player;
+                move_rigth = false;
                 break;
             }
             else {
                 cout << "pole_zanayto";
-                your_move = true;
+                move_rigth = true;
                 break;
             }
         }
     }
     
     void output() {
-        int tempPole[9];
-        string pOle;
+        int temp_field[9];
+        string str_field;
 
         for (size_t i = 0; i < 9; i++)
         {
-            tempPole[i] = polEEE[i];
+            temp_field[i] = pfield[i];
         }
 
         for (size_t i = 0; i < 9; i++)
         {
-            if (tempPole[i] == 1) {
-                pOle += 'X';
+            if (temp_field[i] == 1) {
+                str_field += 'X';
             }
-            if (tempPole[i] == 2) {
-                pOle += 'O';
+            if (temp_field[i] == 2) {
+                str_field += 'O';
             }
-            if (tempPole[i] == 0) {
-                pOle += ' ';
+            if (temp_field[i] == 0) {
+                str_field += ' ';
             }
         }
-            cout << " | " << pOle[0] << " | " << pOle[1] << " | " << pOle[2] << " | " << endl;
-            cout << " | " << pOle[3] << " | " << pOle[4] << " | " << pOle[5] << " | " << endl;
-            cout << " | " << pOle[6] << " | " << pOle[7] << " | " << pOle[8] << " | " << endl;    
+            cout << " | " << str_field[0] << " | " << str_field[1] << " | " << str_field[2] << " | " << endl;
+            cout << " | " << str_field[3] << " | " << str_field[4] << " | " << str_field[5] << " | " << endl;
+            cout << " | " << str_field[6] << " | " << str_field[7] << " | " << str_field[8] << " | " << endl;
     }
 
     void bot_move() {
-        if (Pkr_or_nl == 1) {
-            Bkr_or_nl = 2;
+        if (Player == 1) {
+            Bot = 2;
         }
         else {
-            Bkr_or_nl = 1;
+            Bot = 1;
         }
-        while (!your_move) {
+        while (!move_rigth) {
+            
             srand((unsigned)time(0));
             int ran = (rand() % 9);
             position = ran;
-            if (polEEE[position] == 0) {
-                polEEE[position] = Bkr_or_nl;
-                your_move = true;
+            if (pfield[position] == 0) {
+                pfield[position] = Bot;
+                move_rigth = true;
                 break;
             }
             else {
                 cout << "pole_zanayto";
-                your_move = false;
+                move_rigth = false;
                 break;
             }
+        }
+    }
+
+    void bot_analizer() {
+
+    }
+
+    void game_over_checker() {
+        
+        int temp_field[9];
+        int temp_matrix[3][3];
+
+        for (size_t i = 0; i < 9; i++)
+        {
+            temp_field[i] = pfield[i];
+        }
+        ///////////////////////////////
+        for (int x = 0; x < 3; ++x)
+        {
+            for (int k = 0; k < 3; ++k) {
+                temp_matrix[x][k] = temp_field[3 * x + k];
+            }
+        }
+        ///////////////////////////////
+        for (int i = 0; i < 3; i++)
+        {
+
+            if ((temp_matrix[i][0] == temp_matrix[i][1] == temp_matrix[i][2])||
+                (temp_matrix[0][i] == temp_matrix[1][i] == temp_matrix[2][i])||
+                (temp_matrix[0][0] && temp_matrix[1][1] && temp_matrix[2][2])||
+                (temp_matrix[2][2] && temp_matrix[1][1] && temp_matrix[0][0]))
+            {
+                    
+
+            } 
         }
     }
 
@@ -102,24 +138,25 @@ public:
             system("cls");
             bot_move();
             output();
+            game_over_checker();
         }
     }
 
-    int pole[9]{ 0,0,0,0,0,0,0,0,0 };
-    int* polEEE = pole;
+private:
+    
+    int field[9]{0,0,0,0,0,0,0,0,0};
+    int* pfield = field;
 
-    int position_analizer = 0;
+    int best_move_analizer[9]{ 1,0,1,0,2,0,1,0,1 };
 
-    bool your_move;
+    bool move_rigth;
+
     int position = 0;
-    int Pkr_or_nl = 0;
-    int Bkr_or_nl;
+
+    int Player = 0;
+    int Bot;
 
     bool gameOver = false;
-
-    void analizer() {
-
-    }
 };
 
 int main()
